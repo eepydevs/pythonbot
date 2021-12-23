9d58275597c78b016b8708a4c6d0ba027f9a0f25
Number1GS <72150634+1randomguyspecial@users.noreply.github.com>
2021-12-23 14:26:37 +0000

    added 2 commands in social cog

--- cogs/social.py
+++ cogs/social.py
@@ -82,7 +82,7 @@ class Social(commands.Cog):
           e.set_thumbnail(url = member.avatar)
           await ctx.send(embed = e)
         else:
-          e = discord.Embed(title = f"{member.name}'s posts:", description = "posts here", color = random.randint(0, 16777215))
+          e = discord.Embed(title = f"{member.name}'s posts:", description = f"Posts here\nSubcribers count: {len(db['subs'][str(member.id)])}", color = random.randint(0, 16777215))
           e.set_thumbnail(url = member.avatar)
           e.add_field(name = "No posts here", value = "Check later!")
           await ctx.send(embed = e)
@@ -99,7 +99,7 @@ class Social(commands.Cog):
           names = list1[-1]
           texts = ""
           texts = list2[-1]
-          e = discord.Embed(title = f"{ctx.author.name}'s posts:", description = "posts here", color = random.randint(0, 16777215))
+          e = discord.Embed(title = f"{ctx.author.name}'s posts:", description = f"Posts here\nSubcribers count: {len(db['subs'][str(ctx.author.id)])}", color = random.randint(0, 16777215))
           e.add_field(name = names, value = texts)
           for i in range(len(list1) - 1):
             names = f"{list1[-i - 2]}"
@@ -141,6 +141,56 @@ class Social(commands.Cog):
       else:
         e = discord.Embed(title = "Error", description = "They have no account...", color = random.randint(0, 16777215))
         await ctx.send(embed = e)
+
+  #subscribe command
+  @commands.command(aliases = ["sub"], help = "Subscribe to people", description = "Subscribe to people\nAccount required")
+  async def subscribe(self, ctx, member: discord.Member = None):
+    if member != None:      
+      if str(member.id) in db["account"]:
+        if str(ctx.author.id) in db["account"]:
+          if str(ctx.author.id) not in db["subs"][str(member.id)]:
+            updatelist = db["subs"][str(member.id)]
+            updatelist.append(str(ctx.author.id))
+            db["subs"][str(member.id)] = updatelist
+            e = discord.Embed(title = "Success", description = f"Subscribed to `{member.name}`!", color = random.randint(0, 16777215))
+            await ctx.send(embed = e)
+          else:
+            e = discord.Embed(title = "Error", description = "You're already subscribed to this person!", color = random.randint(0, 16777215))
+            await ctx.send(embed = e)
+        else:
+          e = discord.Embed(title = "Error", description = "You have no account...", color = random.randint(0, 16777215))
+          await ctx.send(embed = e)
+      else:
+        e = discord.Embed(title = "Error", description = "They have no account...", color = random.randint(0, 16777215))
+        await ctx.send(embed = e)
+    else:
+      e = discord.Embed(title = "Error", description = "Mention a person!", color = random.randint(0, 16777215))
+      await ctx.send(embed = e)
+
+  #unsubscribe command
+  @commands.command(aliases = ["unsub"], help = "Subscribe to people", description = "Subscribe to people\nAccount required")
+  async def unsubscribe(self, ctx, member: discord.Member = None):
+    if member != None:
+      if str(member.id) in db["account"]:
+        if str(ctx.author.id) in db["account"]:
+          if str(ctx.author.id) in db["subs"][str(member.id)]:
+            updatelist = list(db["subs"][str(member.id)])
+            updatelist.remove(str(ctx.author.id))
+            db["subs"][str(member.id)] = updatelist
+            e = discord.Embed(title = "Success", description = f"Unsubscribed from `{member.name}`!", color = random.randint(0, 16777215))
+            await ctx.send(embed = e)
+          else:
+            e = discord.Embed(title = "Error", description = "You're already unsubscribed from this person!", color = random.randint(0, 16777215))
+            await ctx.send(embed = e)
+        else:
+          e = discord.Embed(title = "Error", description = "You have no account...", color = random.randint(0, 16777215))
+          await ctx.send(embed = e)
+      else:
+        e = discord.Embed(title = "Error", description = "They have no account...", color = random.randint(0, 16777215))
+        await ctx.send(embed = e)
+    else:
+      e = discord.Embed(title = "Error", description = "Mention a person!", color = random.randint(0, 16777215))
+      await ctx.send(embed = e)
 
 def setup(bot):
   bot.add_cog(Social(bot))
