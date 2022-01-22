6a192d77b68b0839027c46fbfed15374d38f2202
Number1GS <72150634+1randomguyspecial@users.noreply.github.com>
2022-01-22 17:11:14 +0000

    finally changed the site a bit

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask("")
 
 @app.route('/')
 def home():
-  return "Server is ready!"
+  return render_template("index.html")
 
 def run():
   app.run(host = '0.0.0.0', port = 8080)
