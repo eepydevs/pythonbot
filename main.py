edd89d9f9e26782efe621b37c99665e8cb524c74
Number1GS <72150634+1randomguyspecial@users.noreply.github.com>
2022-01-25 12:12:32 +0000

    slash commands available only in some servers because of an issue

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from replit import db
 from disnake.ext import commands
 from server import keep_alive
 
-bot = commands.Bot(command_prefix = lambda bot, msg: (commands.when_mentioned_or(db['prefix'][str(msg.guild.id)]) if msg.guild != None else commands.when_mentioned_or('pb!'))(bot, msg)) #, test_guilds = [908099219401883670, 929889688746086440, 823959191894491206, 866689038731313193]
+bot = commands.Bot(command_prefix = lambda bot, msg: (commands.when_mentioned_or(db['prefix'][str(msg.guild.id)]) if msg.guild != None else commands.when_mentioned_or('pb!'))(bot, msg), intents=discord.Intents.all(), test_guilds = [908099219401883670, 929889688746086440, 823959191894491206, 866689038731313193, 916407122474979398, 926443840632676412]) #, test_guilds = [908099219401883670, 929889688746086440, 823959191894491206, 866689038731313193, 916407122474979398, 926443840632676412]
 
 class EmbedMinimalHelp(commands.MinimalHelpCommand):
   async def send_pages(self):
